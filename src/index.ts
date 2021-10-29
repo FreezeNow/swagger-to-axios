887e6df81cba1c1b9146f71afa14a148f8417072
FreezeNow <yangchenglei@jixin.com>
2021-10-29 14:54:36 +0800

    现在同时支持 yaml 和 json 格式的 swagger 文档了；修复了不存在基础 tags 时无法生成文件的问题

--- src/index.ts
+++ src/index.ts
@@ -53,6 +53,8 @@ const createApiFiles = async (
   swaggerList: {
     /** swagger 文档地址 */
     url: string;
+    /** swagger 文档文件类型， yaml 还是 json ，默认为 yaml */
+    urlType: string;
     /** 生成文件后该文档的文件夹名称 */
     name: string;
   }[] = [],
@@ -87,6 +89,9 @@ const createApiFiles = async (
       const json = await axios
         .get(element.url)
         .then((res) => {
+          if (element.urlType && element.urlType === 'json') {
+            return res.data;
+          }
           const jsonData: any = YAML.load(res.data);
           return jsonData;
         })
@@ -99,12 +104,14 @@ const createApiFiles = async (
         list: [],
       };
       // 获取文档的 tags，并插入文件夹对象的 list 字段中
-      for (const tag of json.tags) {
-        folderObj.list.push({
-          name: tag.name,
-          comment: tag.description,
-          list: [],
-        });
+      if (json?.tags) {
+        for (const tag of json.tags) {
+          folderObj.list.push({
+            name: tag.name,
+            comment: tag.description,
+            list: [],
+          });
+        }
       }
       // 循环文档的 paths，取出 api 地址、类型、描述并放入 tag list 中
       for (const path in json.paths) {
@@ -126,6 +133,18 @@ const createApiFiles = async (
                     comment: [method.summary],
                   });
                 }
+              } else {
+                folderObj.list.push({
+                  name: method.tags[0],
+                  comment: '',
+                  list: [
+                    {
+                      url: path,
+                      method: [name],
+                      comment: [method.summary],
+                    },
+                  ],
+                });
               }
             }
           }
@@ -139,21 +158,31 @@ const createApiFiles = async (
     for (const folder of swagger.list) {
       await fsPromises.mkdir(`${swagger.path}/${folder.name}`, { recursive: true });
       for (const file of folder.list) {
-        let fileContent = `// ${file.comment}
-const baseURL = '${folder.baseURL}';
+        let fileContent = '';
+        // 如果包含注释，则在文件顶部添加注释
+        if (file.comment) {
+          fileContent += `// ${file.comment}
+`;
+        }
+        fileContent += `const baseURL = '${folder.baseURL}';
 const host = \`${
-          folder.host.includes('127.0.0.1')
+          folder.host && folder.host.includes('127.0.0.1')
             ? folder.host
             : "${process.env.VUE_APP_HOST ? process.env.VUE_APP_HOST : '基于隐私考虑，已隐藏默认ip'}"
         }\`;
-`;
+${
+  improtAxiosPath
+    ? `import request from '${improtAxiosPath}';
+`
+    : ''
+}`;
+
         const apiList = file.list;
         for (const api of apiList) {
           for (let l = 0; l < api.method.length; l++) {
             const method = api.method[l];
             fileContent += `
 //  ${api.comment[l]}
-${improtAxiosPath ? `import request from ${improtAxiosPath}` : ''}
 export function ${method.toLowerCase() + url2name(api.url)}(${
               method.toUpperCase() === 'GET' ? 'params' : 'data'
             }, options) {
